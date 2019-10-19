c832ced1259a3fd202a482aadca08ea5f45678ca
DuongYi <nashurato11122@gmail.com>
2019-10-19 20:35:59 +0700

    add all

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+import java.io.InputStream;
 import java.util.Optional;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -47,7 +46,42 @@ public class Main extends Application {
         });
 
         VBox menuOption = new VBox();
-        
+        menuOption.setPrefWidth(100);
+        menuOption.setPrefHeight(50);
+        Button startButton = new Button("Start");
+        Button continueButton = new Button("Continue");
+        Button settingButton = new Button("Setting");
+        Button exitButton = new Button("Exit");
+
+        startButton.setMinWidth(menuOption.getPrefWidth());
+        continueButton.setMinWidth(menuOption.getPrefWidth());
+        settingButton.setMinWidth(menuOption.getPrefWidth());
+        exitButton.setMinWidth(menuOption.getPrefWidth());
+
+        startButton.setMinHeight(menuOption.getPrefHeight());
+        continueButton.setMinHeight(menuOption.getPrefHeight());
+        settingButton.setMinHeight(menuOption.getPrefHeight());
+        exitButton.setMinHeight(menuOption.getPrefHeight());
+        Label label = new Label("TOWER DEFENSE PRO");
+        label.setFont(new Font(40));
+        label.setTextFill(Color.DARKORANGE);
+
+        menuOption.getChildren().addAll(label,startButton,continueButton,settingButton,exitButton);
+        menuOption.setSpacing(10);
+        menuOption.setAlignment(Pos.CENTER);
+
+        InputStream is;
+        Image backgroud = new Image("./resources/img/MainBackground.jpg");
+        ImageView imageView = new ImageView(backgroud);
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(600);
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(imageView, menuOption);
+
+        Scene scene = new Scene(stackPane, 600, 400);
+        stage.setScene(scene);
+        stage.show();
 
 
     }
